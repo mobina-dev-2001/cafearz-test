f1696bb696493f95ea0a2dca62a3006e94b42e0d
insomnish dev <mobina.smt@gmail.com>
2025-08-16 15:41:04 +0330

    chore: add Stylis

---
+++ dist-node/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "src/": path.resolve(__dirname, "src/") + "/",
+        },
+    },
+});
